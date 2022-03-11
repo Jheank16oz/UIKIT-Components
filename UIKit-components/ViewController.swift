6952202017aa9f186d916d311ae4fef30b64bba9
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-03-11 10:00:38 -0500

    Initial project with stack view and navigation controller

---
+++ UIKit-components/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKit-components
+//
+//  Created by jpineros on 17/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+}
+
