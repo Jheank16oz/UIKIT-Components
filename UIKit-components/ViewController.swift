0f14c906967b452795f915b0b1dd8e4549c1b4b0
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-03-23 08:03:32 -0500

    Master Detail

--- UIKit-components/ViewController.swift
+++ UIKit-components/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sender:String? = nil
+
+    @IBSegueAction func detailInfo(_ coder: NSCoder, sender: Any?, segueIdentifier: String?) -> ViewController? {
+        
+        if segueIdentifier == "SequeStackView" {
+            print("detail for stacks")
+        }
+        return ViewController(coder: coder)
+    }
+    private var selectedInfo = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+       
+        
     }
     
 }
